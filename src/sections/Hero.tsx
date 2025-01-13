c19a2795836083a8eff3e3585e9baacc543997af
prithvikings <prithvi07raj07@gmail.com>
2025-01-13 17:01:50 +0530

    Refactor Hero section styles and improve responsiveness

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,19 +9,19 @@ export default function Hero() {
                         ✨ Open Source{" "}
                     </div>
                 </div>
-                <h1 className=" text-6xl md:text-7xl font-medium text-center mt-6">
+                <h1 className=" text-6xl md:text-7xl font-medium text-center mt-6 lg:text-8xl">
                     Impactful design, created effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
                     Design tools should&apos;t slow down. Layers combines
                     powerful features with an intrutive interface with you in
                     your creative flow.
                 </p>
-                <form className="flex border border-white/15 rounded-full p-2 mt-8">
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 max-w-lg mx-auto">
                     <input
                         type="email"
                         placeholder="Enter Your Email"
-                        className="bg-transparent px-4"
+                        className="bg-transparent px-4 md:flex-1"
                     />
                     <Button
                         type="submit"
