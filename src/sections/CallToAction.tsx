ff6d4a3dc884ec74981165042d6c2489cc80a128
prithvikings <prithvi07raj07@gmail.com>
2025-01-14 19:34:29 +0530

    completed with design structure

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -3,12 +3,14 @@ export default function CallToAction() {
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
                 <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
-                {Array.from({ length: 10 }).map((_, index) => (
-                    <div key={index} className="flex items-center gap-16">
-                        <span className="text-lime-400 text-7xl">&#10038;</span>
-                        <span>Try it for free</span>
-                    </div>
-                ))}
+                    {Array.from({ length: 10 }).map((_, index) => (
+                        <div key={index} className="flex items-center gap-16">
+                            <span className="text-lime-400 text-7xl">
+                                &#10038;
+                            </span>
+                            <span>Try it for free</span>
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import { section } from "framer-motion/client";
+import logoimage from "@/assets/images/logo.svg";
+import Image from "next/image";
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,22 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section>
+            <div className="container">
+                <div className="flex flex-col md:flex-row md:justify-between items-center gap-6">
+                    <div>
+                        <Image src={logoimage} alt="logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a key={link.label} className="text-white/50 text-sm">{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
