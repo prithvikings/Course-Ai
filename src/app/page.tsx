b450c5080f38f84c7edfff5a4e9bf7b40136da0c
prithvikings <prithvi07raj07@gmail.com>
2025-01-14 11:09:01 +0530

    Refactor code to add Integrations and Faqs sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "@/sections/Hero";
 import LogoTicker from "@/sections/LogoTicker";
 import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
+import Integrations from "@/sections/Integrations";
+import Faqs from "@/sections/Faqs";
 export default function Home() {
     return (
         <>
@@ -12,6 +14,8 @@ export default function Home() {
             <LogoTicker />
             <Introduction />
             <Features />
+            <Integrations />
+            <Faqs />
         </>
 
     )
